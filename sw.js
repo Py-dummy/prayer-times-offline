bd3b508fbce45ee69fcf437f83d836949ef7449e
Py-dummy <imamhasanchowdhury01@gmail.com>
2026-04-17 11:44:07 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'prayer-v1.0.5';
+const urlsToCache = ['./']; // caches the main HTML file
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => 
+      response || fetch(event.request).catch(() => new Response('Offline', { status: 503 }))
+    )
+  );
+});
